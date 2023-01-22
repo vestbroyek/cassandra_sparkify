1ccdf297ee090a182d1635baf7c2063ff3ed46d7
Maurits <mcb.westbroek@gmail.com>
2023-01-22 08:48:38 +0000

    Add transformation logic

--- etl.py
+++ etl.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import cassandra
-from cassandra.cluster import Cluster
 import re
 import os
 import glob
@@ -15,4 +13,31 @@ filepath = os.getcwd() + '/event_data'
 for root, dirs, files in os.walk(filepath):
     file_path_list = glob.glob(os.path.join(root,'*'))
 
-pass
+# initiating an empty list of rows that will be generated from each file
+full_data_rows_list = [] 
+    
+# for every filepath in the file path list 
+for f in file_path_list:
+
+# reading csv file 
+    with open(f, 'r', encoding = 'utf8', newline='') as csvfile: 
+        csvreader = csv.reader(csvfile) 
+        next(csvreader)
+        
+ # extracting each data row one by one and append it        
+        for line in csvreader:
+            full_data_rows_list.append(line) 
+
+print(len(full_data_rows_list))
+
+# creating a smaller event data csv file called event_datafile_full
+csv.register_dialect('myDialect', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+
+with open('event_datafile_new.csv', 'w', encoding = 'utf8', newline='') as f:
+    writer = csv.writer(f, dialect='myDialect')
+    writer.writerow(['artist','firstName','gender','itemInSession','lastName','length',\
+                'level','location','sessionId','song','userId'])
+    for row in full_data_rows_list:
+        if (row[0] == ''):
+            continue
+        writer.writerow((row[0], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[12], row[13], row[16]))
