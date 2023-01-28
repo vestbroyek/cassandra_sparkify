b72902ccab28e00222764b968062909c9621361a
Maurits <mcb.westbroek@gmail.com>
2023-01-28 16:59:16 +0000

    Refactor ETL

--- etl.py
+++ etl.py
@@ -6,38 +6,38 @@ import numpy as np
 import json
 import csv
 
-# create list of filepaths 
-filepath = os.getcwd() + '/event_data'
+def run_etl():
 
-# create a for loop to create a list of files and collect each filepath
-for root, dirs, files in os.walk(filepath):
-    file_path_list = glob.glob(os.path.join(root,'*'))
+    # create list of filepaths 
+    filepath = os.getcwd() + '/event_data'
 
-# initiating an empty list of rows that will be generated from each file
-full_data_rows_list = [] 
-    
-# for every filepath in the file path list 
-for f in file_path_list:
+    # create a for loop to create a list of files and collect each filepath
+    for root, dirs, files in os.walk(filepath):
+        file_path_list = glob.glob(os.path.join(root,'*'))
 
-# reading csv file 
-    with open(f, 'r', encoding = 'utf8', newline='') as csvfile: 
-        csvreader = csv.reader(csvfile) 
-        next(csvreader)
+    # initiating an empty list of rows that will be generated from each file
+    full_data_rows_list = [] 
         
- # extracting each data row one by one and append it        
-        for line in csvreader:
-            full_data_rows_list.append(line) 
+    # for every filepath in the file path list 
+    for f in file_path_list:
 
-print(len(full_data_rows_list))
+    # reading csv file 
+        with open(f, 'r', encoding = 'utf8', newline='') as csvfile: 
+            csvreader = csv.reader(csvfile) 
+            next(csvreader)
+            
+    # extracting each data row one by one and append it        
+            for line in csvreader:
+                full_data_rows_list.append(line) 
 
-# creating a smaller event data csv file called event_datafile_full
-csv.register_dialect('myDialect', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    # creating a smaller event data csv file called event_datafile_full
+    csv.register_dialect('myDialect', quoting=csv.QUOTE_ALL, skipinitialspace=True)
 
-with open('event_datafile_new.csv', 'w', encoding = 'utf8', newline='') as f:
-    writer = csv.writer(f, dialect='myDialect')
-    writer.writerow(['artist','firstName','gender','itemInSession','lastName','length',\
-                'level','location','sessionId','song','userId'])
-    for row in full_data_rows_list:
-        if (row[0] == ''):
-            continue
-        writer.writerow((row[0], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[12], row[13], row[16]))
+    with open('event_datafile_new.csv', 'w', encoding = 'utf8', newline='') as f:
+        writer = csv.writer(f, dialect='myDialect')
+        writer.writerow(['artist','firstName','gender','itemInSession','lastName','length',\
+                    'level','location','sessionId','song','userId'])
+        for row in full_data_rows_list:
+            if (row[0] == ''):
+                continue
+            writer.writerow((row[0], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[12], row[13], row[16]))
